4b4418c3fb304315259bf2fd70dcbf73dce2b33c
CollinsRollinsDev <collinsrollins07@gmail.com>
2022-01-02 07:53:33 +0100

    fixing build bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ThemeProvider forcedTheme={false} defaultTheme = 'system'>
+  return <ThemeProvider defaultTheme = 'system'>
   <Component {...pageProps} />
 </ThemeProvider>
 }
